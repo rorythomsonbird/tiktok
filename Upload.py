5dd43a7fdc63af45c79f28dee6b4e0634f9e565c
rorythomsonbird <rorythomsonbird@gmail.com>
2025-01-01 15:01:05 +0000

    add automatic log in

--- Upload.py
+++ Upload.py
@@ -7,46 +7,46 @@ import time
 
 class Upload:
     def send_message():
-        url = f"https://www.google.com/?zx=1735652103740&no_sw_cr=1" #URL
+        
+        url = f"https://www.tiktok.com/" #URL
         options = webdriver.ChromeOptions()
         options = ChromeOptions()
-        options.add_argument('--user-agent=Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/131.0.6778.204 Safari/537.3')
+        options.add_argument('user-agent=Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/131.0.6778.204 Safari/537.3')
         options.add_argument('--window-size=1920x1080')
         
         
         
-        #options.add_argument('--disable-gpu')
-        #options.add_argument('--disable-extensions')
-        #options.add_argument('--ignore-ssl-errors=yes')
-        #options.add_argument('--ignore-certificate-errors')
         driver = webdriver.Chrome(options=options)
         driver = Chrome(options=options)
+        
         driver.get(url)
-        time.sleep(3)
-        #signin = driver.find_element(By.ID, 'menu-button-:r15:')
-        #signin.click()
-        time.sleep(10)
-        time.sleep(90000000000)
-        #Get around the focus guard
-        # guarddelete = driver.find_element(By.CSS_SELECTOR,"div[data-focus-guard='true'][tabindex='0'][style='width: 1px; height: 0px; padding: 0px; overflow: hidden; position: fixed; top: 1px; left: 1px;'][data-aria-hidden='true'][aria-hidden='true']")
-        # driver.execute_script("arguments[0].parentNode.removeChild(arguments[0]);", guarddelete)
-        # time.sleep(6)
-        # guarddelete = driver.find_element(By.CSS_SELECTOR,"div.chakra-portal[data-aria-hidden='true'][aria-hidden='true']")
-        # driver.execute_script("arguments[0].parentNode.removeChild(arguments[0]);", guarddelete)
-        #email = 
-        #email.send_keys("wormywormman2@gmail.com")
-        time.sleep(30)
-        # textarea = driver.find_element('css selector', 'textarea[data-testid="chat-input-textarea"]')
-        # button = driver.find_element('css selector', 'button[aria-label="Send"]')
-        # time.sleep(5)
-        # email = driver.find_element(By.CSS_SELECTOR,"input.chakra-input.css-z7n099")
-        # email.send_keys("wormywormman2@gmail.com")
-        # time.sleep(3)
-        # emailbutton = driver.find_element(By.CSS_SELECTOR,"button.chakra-button.css-1p10gbu")
-        # emailbutton.click()
-        # time.sleep(3000)
-        # textarea = driver.find_element(By.CSS_SELECTOR, 'textarea[data-testid="chat-input-textarea"]')
-        # button = driver.find_element(By.CSS_SELECTOR, 'button[aria-label="Send"]')
+        try:
+            time.sleep(3)
+            signin = driver.find_element(By.ID, 'header-login-button')
+            signin.click()
+            time.sleep(4)
+            selectbyemailphone = driver.find_element(By.XPATH, "//div[@role='link' and @data-e2e='channel-item']//div[contains(@class, 'css-1cp64nz-DivTextContainer')]//div[text()='Use phone / email / username']")
+            time.sleep(3)
 
-        # textarea.send_keys("Hello")
-        # button.click()
+            selectbyemailphone.click()
+            selectbyemail = WebDriverWait(driver,10).until(EC.presence_of_element_located((By.XPATH, "//div[@class='css-1usm4ad-DivDescription e1521l5b3']/a[contains(@href, '/login/phone-or-email/email')]")))
+            selectbyemail.click()
+            time.sleep(2)
+            userfield = WebDriverWait(driver,10).until(EC.presence_of_element_located((By.NAME, "username"))) 
+            creds = open("credentials.txt","r")
+            credline = creds.read().split("\n")
+
+            time.sleep(4)
+            userfield.send_keys(credline[0].split(",")[1])
+            passfield = WebDriverWait(driver,10).until(EC.presence_of_element_located((By.CSS_SELECTOR, "input[type='password'][placeholder='Password']"))) 
+            passfield.send_keys(credline[1].split(",")[1])
+            button = WebDriverWait(driver,10).until(EC.presence_of_element_located((By.CSS_SELECTOR, "button[data-e2e='login-button']")))
+            button.click()
+            time.sleep(30)
+            driver.quit()
+        except Exception:
+            driver.quit()
+
+        
+        time.sleep(30)
+       
